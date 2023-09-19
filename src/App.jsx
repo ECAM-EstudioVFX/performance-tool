151dbc58403f673a793530d12a6390386a01393f
Pablo Fernando Mesquida <pablo.mesquida@ecam.es>
2023-09-19 10:03:21 +0200

    refactor: Model component - Implement Stage from Drei instead useEffect for center de model and remove lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,91 @@
-import React, { useEffect, useState, Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
-import Model from "./components/Model";
-import { Perf } from "r3f-perf";
-import * as THREE from "three";
-import { Spinner } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
-import "./App.css";
+import React, { useEffect, useState, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera, OrbitControls, Stage } from '@react-three/drei'
+import Model from './components/Model'
+import { Perf } from 'r3f-perf'
+import * as THREE from 'three'
+import { Spinner } from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
+import './App.css'
 
 function App() {
-  const [modelURL, setModelURL] = useState(null);
-  const [showSpinner, setShowSpinner] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const [modelURL, setModelURL] = useState(null)
+  const [showSpinner, setShowSpinner] = useState(false)
+  const [darkMode, setDarkMode] = useState(false)
 
-  const separation = 20;
+  const separation = 20
 
   useEffect(() => {
     if (darkMode) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add('dark')
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove('dark')
     }
-  }, [darkMode]);
+  }, [darkMode])
 
   const handleFileChange = (event) => {
-    setShowSpinner(true);
-    const file = event.target.files[0];
+    setShowSpinner(true)
+    const file = event.target.files[0]
     if (file) {
-      const objectURL = URL.createObjectURL(file);
-      setModelURL(objectURL);
+      const objectURL = URL.createObjectURL(file)
+      setModelURL(objectURL)
     }
-  };
+  }
 
   return (
     <div
       className={`w-full relative ${
-        darkMode ? "bg-dark-900 text-light-100" : ""
+        darkMode ? 'bg-dark-900 text-light-100' : ''
       }`}
-      style={{ height: "80vh" }}
+      style={{ height: '80vh' }}
     >
-      <div className="flex items-center justify-between">
+      <div className='flex items-center justify-between'>
         <h1
-          className="text-3xl font-bold underline
+          className='text-3xl font-bold underline
           dark:text-light-100
           text-dark-900
           text-center
           w-full
-          "
+          '
         >
           Prueba de rendimiento
         </h1>
         <Button
           isIconOnly
-          color="danger"
-          aria-label="Like"
+          color='danger'
+          aria-label='Like'
           onClick={() => setDarkMode((prev) => !prev)}
         >
           {darkMode ? (
             <img
-              src="https://img.icons8.com/?size=128&id=0pAges62zAXE&format=png"
-              className="w-8"
+              src='https://img.icons8.com/?size=128&id=0pAges62zAXE&format=png'
+              className='w-8'
             />
           ) : (
             <img
-              src="https://img.icons8.com/?size=128&id=EJa7ybjieiOc&format=png"
-              className="w-8"
+              src='https://img.icons8.com/?size=128&id=EJa7ybjieiOc&format=png'
+              className='w-8'
             />
           )}
         </Button>
       </div>
-      <input type="file" onChange={handleFileChange} accept=".glb, .gltf" />
+      <input type='file' onChange={handleFileChange} accept='.glb, .gltf' />
       {showSpinner && (
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center  bg-opacity-50">
-          <Spinner color="warning" size="lg" />
+        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center  bg-opacity-50'>
+          <Spinner color='warning' size='lg' />
         </div>
       )}
 
       <Canvas
-        style={{ border: darkMode ? "1px solid white" : "1px solid black" }}
-        className="mt-5"
+        style={{ border: darkMode ? '1px solid white' : '1px solid black' }}
+        className='mt-5'
         camera={{ position: [10, 0, 10], fov: 75, near: 0.1, far: 1000 }}
         onCreated={({ gl }) => {
-          gl.shadowMap.enabled = true;
-          gl.shadowMap.type = THREE.PCFSoftShadowMap;
+          gl.shadowMap.enabled = true
+          gl.shadowMap.type = THREE.PCFSoftShadowMap
         }}
       >
-        <Perf position="top-left" />
+        <Perf position='top-left' />
         <ambientLight intensity={0.5} />
         <directionalLight position={[0, 10, 5]} intensity={1} castShadow />
         <PerspectiveCamera makeDefault position={[0, 0, 15]} />
@@ -96,17 +96,19 @@ function App() {
         <Suspense fallback={null}>
           {modelURL && (
             <>
-              <Model
-                url={modelURL}
-                onLoad={() => setShowSpinner(false)}
-                position={{ x: 10, y: 0, z: 0 }}
-              />
+              <Stage>
+                <Model
+                  url={modelURL}
+                  onLoad={() => setShowSpinner(false)}
+                  position={{ x: 10, y: 0, z: 0 }}
+                />
+              </Stage>
             </>
           )}
         </Suspense>
       </Canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,35 +1,16 @@
-import React, { useEffect, useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import * as THREE from "three";
-import propTypes from "prop-types";
+import React, { useEffect, useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import propTypes from 'prop-types'
 
 function Model({ url, onLoad, position }) {
-  const meshRef = useRef();
-  const { nodes, materials } = useGLTF(url);
-  const { camera } = useThree();
-
-  useEffect(() => {
-    if (meshRef.current) {
-      const box = new THREE.Box3().setFromObject(meshRef.current);
-      const size = box.getSize(new THREE.Vector3());
-      const center = box.getCenter(new THREE.Vector3());
-
-      const desiredSize = 5;
-      const scaleValue = desiredSize / Math.max(size.x, size.y, size.z);
-      meshRef.current.scale.set(scaleValue, scaleValue, scaleValue);
-
-      meshRef.current.position.set(position.x, position.y, position.z);
-      camera.position.set(position.x, position.y, position.z + desiredSize);
-      camera.lookAt(center);
-    }
-  }, [camera]);
+  const meshRef = useRef()
+  const { nodes, materials } = useGLTF(url)
 
   return (
     <group ref={meshRef} onAfterRender={onLoad()}>
       {Object.entries(nodes).map(
         ([name, node]) =>
-          node.type === "Mesh" && (
+          node.type === 'Mesh' && (
             <primitive
               key={name}
               object={node}
@@ -38,13 +19,13 @@ function Model({ url, onLoad, position }) {
           )
       )}
     </group>
-  );
+  )
 }
 
 Model.propTypes = {
   url: propTypes.string.isRequired,
   onLoad: propTypes.func,
-  position: propTypes.arrayOf(propTypes.number),
-};
+  position: propTypes.object.isRequired
+}
 
-export default Model;
+export default Model
